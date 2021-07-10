ba61b541e4c9b43d69b6e78adff0365be90a6b9e
alamin <alamin@gmail.com>
2021-07-10 08:26:44 +0600

    add public folder for static file

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", require("./routes/weather"));
+// Middleware
+app.use(express.static("public"));
+
+// Use view engine
+app.set("view engine", "ejs");
+
+// Middleware routes
+app.use("/", require("./routes/weatherRoute"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- routes/weatherRoute.js
+++ routes/weatherRoute.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello express");
+  res.render("index");
 });
 
 module.exports = router;
